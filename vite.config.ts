a4570cc097a9be46c6e1340ab821f7a700a8703d
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-01-07 00:51:46 +0530

    Added generation of 404.html (which will be copy of index.html)

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,33 @@
-import { defineConfig } from "vite";
+import { defineConfig, type Plugin, type ResolvedConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import fs from "node:fs";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-
+    plugins: [react(), copy404HTMLVitePlugin()],
     build: {
         outDir: "dist",
     },
     base: "./",
 });
+
+function copy404HTMLVitePlugin(): Plugin {
+    let config: ResolvedConfig;
+
+    return {
+        name: "copy-404",
+        configResolved: (resolvedConfig: ResolvedConfig) => {
+            config = resolvedConfig;
+        },
+        closeBundle: () => {
+            if (config !== undefined) {
+                const outDir = config.build.outDir;
+                fs.copyFileSync(
+                    path.join(outDir, "index.html"),
+                    path.join(outDir, "404.html"),
+                );
+            }
+        },
+    };
+}
